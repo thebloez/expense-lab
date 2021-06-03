f80efdccbff6c8f03b4e74aba5af0e6f30c1122c
thebloez <ryandunksfamz@gmail.com>
2021-06-03 20:53:21 +0700

    enable JPA Auditing

--- src/main/java/thebloez/lab/expense/ExpenseApplication.java
+++ src/main/java/thebloez/lab/expense/ExpenseApplication.java
@@ -2,7 +2,9 @@ package thebloez.lab.expense;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ExpenseApplication {
 
